76a193e4b9d82e7ace5b43788b29faeb311232db
Ho Thi Thom <thom.ho@internal.banvien.com>
2025-01-02 11:45:35 +0700

    sửa script test login lỗi timeout

--- src/fixture/overview.fixture.ts
+++ src/fixture/overview.fixture.ts
@@ -1,18 +1,20 @@
-import {test as base} from '@playwright/test';
+import {expect, test as base} from '@playwright/test';
 import { LoginPage } from "../pom/login.page";
 import { OverviewPage } from '../pom/overview.page';
+import { Expect } from '@playwright/test';
 
 
 const test = base.extend <{overviewpage: OverviewPage}> ({
 
 overviewpage: async ({page},use) => {
 
-await page.goto('https://qa-nexthcm.banvien.com.vn/login');
+await page.goto('https://qa-nexthcm.banvien.com.vn/login', {timeout:60000 });
 
 const loginpage = new LoginPage(page);
 await loginpage.fillInformation("son.nguyen","BVC@12345678");
 await loginpage.Login();
 
+await expect(page).toHaveURL('https://qa-nexthcm.banvien.com.vn/overview');
 const overviewpage = new OverviewPage(page);
 await use(overviewpage);
 
