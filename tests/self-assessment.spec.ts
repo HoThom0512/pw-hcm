0cfefbcc88e061686f8e18e2bba8e5a39aa5df86
Ho Thi Thom <thom.ho@internal.banvien.com>
2025-02-14 15:41:09 +0700

    thêm các file

--- tests/self-assessment.spec.ts
+++ tests/self-assessment.spec.ts
@@ -21,17 +21,23 @@ test('step 1 login on HCM system', async({useroverviewpage,page}) => {
   await page.locator('div[id="demo-select-selfAssessment.0.self_assessment_score"]').click();
   
   // Chọn giá trị '1' từ dropdown đầu tiên
-  await page.locator('div[id="demo-select-selfAssessment.0.self_assessment_score"] >> div.ant-select-item-option:nth-child(3)').click();
-  
-  // Click vào text 'Weight' để đóng dropdown
-  await page.click('text= Weight');
+  await page.locator('div#rc_select_0_list >> div.ant-select-item-option:nth-child(2)[title="0.5"]').click();
+
 
   
+  // Click vào text 'Weight' để đóng dropdown
+  await page.click('body');
+ });
+}); 
+
+ 
   // Bước 3: Mở dropdown thứ 3
   await page.locator('div[id="demo-select-selfAssessment.3.self_assessment_score"]').click();
   
   // Chọn giá trị '2' từ dropdown thứ hai
-  await page.locator('div[id="demo-select-selfAssessment.3.self_assessment_score"] >> div.ant-select-item-option:nth-child(4)').click();
+  await page.locator('div#rc_select_0_list').waitFor({ state: 'visible' });
+ await page.locator('div#rc_select_0_list >> div.ant-select-item-option[label="0.5"]').click();
+
   
   // Click vào text 'Weight' để đóng dropdown
   await page.click('text= Weight');
@@ -45,13 +51,14 @@ test('step 1 login on HCM system', async({useroverviewpage,page}) => {
   
   // Click vào text 'Weight' để đóng dropdown
   await page.click('text= Weight');
-
-  */
+*/
+  
 //a.viết 1 hàm thực hiện thao tác mở dropdown, chọn giá trị và đóng dropdown.
 //1. mở dropdown
 //2.chọn giá trị trong dropdown
 //3. click để đóng dropdown
 //b. viết 1 vòng lặp for of dể lưu dropdownid, optionindex
+
 
 async function selectValueDropList(page, dropdownId, optionIndex) {
  await page.locator(`div[id = "${dropdownId}"]`).click();
@@ -61,16 +68,25 @@ async function selectValueDropList(page, dropdownId, optionIndex) {
 }
 
 const idDropList = [
-   {id:'demo-select-selfAssessment.0.self_assessment_score',optionIndex:'1'},
-   {id:'demo-select-selfAssessment.2.self_assessment_score',optionIndex:'2'},
-   {id:'demo-select-selfAssessment.3.self_assessment_score',optionIndex:'3'},
-   {id:'demo-select-selfAssessment.5.self_assessment_score',optionIndex:'4'},
+   {id:'demo-select-selfAssessment.0.self_assessment_score',optionIndex:'1', expectSubScore: '0.5'},
+   {id:'demo-select-selfAssessment.2.self_assessment_score',optionIndex:'2',expectSubScore: '0.45'},
+   {id:'demo-select-selfAssessment.3.self_assessment_score',optionIndex:'3',expectSubScore: '1.4'},
+   {id:'demo-select-selfAssessment.5.self_assessment_score',optionIndex:'4',expectSubScore: '0'},
 
 ]
- for (const SelectIDList of idDropList) {
- await selectValueDropList(page,SelectIDList.id, SelectIDList.optionIndex);
+let toTalSelfAssessment = "";
+ /*for (const SelectIDList of idDropList) {
+ await selectValueDropList(page,SelectIDList.id, SelectIDList.optionIndex); *
 
- }
+ }*/
+for (let i=0; i<=idDropList.length; i++){
+
+  const SelectIDList = idDropList[i];
+  toTalSelfAssessment += idDropList[i];
+}
+
+//kiểm tra totalselfassessment:div.card-score>>div[text()="1.43"]
+ await expect(page.locator(`div.card-score>>div[text()="${toTalSelfAssessment}]`)).toHaveText('1.43');
  await page.locator('//span[text() ="Save as Draft"]').click();
 });
 
