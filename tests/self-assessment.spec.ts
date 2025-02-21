d43aea0b133804828f400b276cc37342dfa9d4c4
Ho Thi Thom <thom.ho@internal.banvien.com>
2025-02-21 16:13:41 +0700

    21/2/2025

--- tests/self-assessment.spec.ts
+++ tests/self-assessment.spec.ts
@@ -15,44 +15,6 @@ test('step 1 login on HCM system', async({useroverviewpage,page}) => {
   });
 
 
-
- /*await test.step('step 4 select score', async () => {
-  // Bước 1: Hover vào phần tử và sau đó click để mở dropdown của dropdown đầu tiên
-  await page.locator('div[id="demo-select-selfAssessment.0.self_assessment_score"]').click();
-  
-  // Chọn giá trị '1' từ dropdown đầu tiên
-  await page.locator('div#rc_select_0_list >> div.ant-select-item-option:nth-child(2)[title="0.5"]').click();
-
-
-  
-  // Click vào text 'Weight' để đóng dropdown
-  await page.click('body');
- });
-}); 
-
- 
-  // Bước 3: Mở dropdown thứ 3
-  await page.locator('div[id="demo-select-selfAssessment.3.self_assessment_score"]').click();
-  
-  // Chọn giá trị '2' từ dropdown thứ hai
-  await page.locator('div#rc_select_0_list').waitFor({ state: 'visible' });
- await page.locator('div#rc_select_0_list >> div.ant-select-item-option[label="0.5"]').click();
-
-  
-  // Click vào text 'Weight' để đóng dropdown
-  await page.click('text= Weight');
-
-
-  // Bước 4: Mở dropdown thứ 4
-  await page.locator('div[id="demo-select-selfAssessment.5.self_assessment_score"]').click();
-  
-  // Chọn giá trị '2' từ dropdown thứ hai
-  await page.locator('div[id="demo-select-selfAssessment.5.self_assessment_score"] >> div.ant-select-item-option:nth-child(4)').click();
-  
-  // Click vào text 'Weight' để đóng dropdown
-  await page.click('text= Weight');
-*/
-  
 //a.viết 1 hàm thực hiện thao tác mở dropdown, chọn giá trị và đóng dropdown.
 //1. mở dropdown
 //2.chọn giá trị trong dropdown
@@ -60,39 +22,59 @@ test('step 1 login on HCM system', async({useroverviewpage,page}) => {
 //b. viết 1 vòng lặp for of dể lưu dropdownid, optionindex
 
 
-async function selectValueDropList(page, dropdownId, optionIndex) {
+/*async function selectValueDropList(page, dropdownId, optionIndex, locatorSubScore, SubScore) {
  await page.locator(`div[id = "${dropdownId}"]`).click();
  await page.locator(`div[id="${dropdownId}"] >> div.ant-select-item-option:nth-child(${optionIndex})`).click();
  await page.locator('body').click();
+ await expect(page.locator(locatorSubScore)).toHaveText(SubScore);
 
 }
 
 const idDropList = [
-   {id:'demo-select-selfAssessment.0.self_assessment_score',optionIndex:'1', expectSubScore: '0.5'},
-   {id:'demo-select-selfAssessment.2.self_assessment_score',optionIndex:'2',expectSubScore: '0.45'},
-   {id:'demo-select-selfAssessment.3.self_assessment_score',optionIndex:'3',expectSubScore: '1.4'},
-   {id:'demo-select-selfAssessment.5.self_assessment_score',optionIndex:'4',expectSubScore: '0'},
+   {id:'demo-select-selfAssessment.0.self_assessment_score',optionIndex:1, locatorSubScore:'//tbody/tr[2]/td[3]/td[1]/text()', SubScore: 0},
+   {id:'demo-select-selfAssessment.2.self_assessment_score',optionIndex:2, locatorSubScore:'//tbody/tr[4]/td[3]/td[1]/text()', SubScore: 0.15},
+   {id:'demo-select-selfAssessment.3.self_assessment_score',optionIndex:3, locatorSubScore:'//tbody/tr[5]/td[3]/td[1]/text()', SubScore: 0.7},
+   {id:'demo-select-selfAssessment.5.self_assessment_score',optionIndex:4, locatorSubScore:'//tbody/tr[7]/td[3]/td[1]/text()', SubScore: -0.15},
 
 ]
-let toTalSelfAssessment = "";
+//let toTalSelfAssessment = "";
  /*for (const SelectIDList of idDropList) {
  await selectValueDropList(page,SelectIDList.id, SelectIDList.optionIndex); *
 
  }*/
-for (let i=0; i<=idDropList.length; i++){
+/*for (let i=0; i< idDropList.length; i++){
 
   const SelectIDList = idDropList[i];
-  toTalSelfAssessment += idDropList[i];
+  
+  await selectValueDropList(page, SelectIDList.id, SelectIDList.optionIndex, SelectIDList.locatorSubScore, SelectIDList.SubScore);
+  
+ 
 }
 
-//kiểm tra totalselfassessment:div.card-score>>div[text()="1.43"]
- await expect(page.locator(`div.card-score>>div[text()="${toTalSelfAssessment}]`)).toHaveText('1.43');
+
  await page.locator('//span[text() ="Save as Draft"]').click();
 });
 
+/*const text1 = await page.locator('//tbody/tr[2]/td[3]/td[1]/text()');
+const text2 = await page.locator('//tbody/tr[4]/td[3]/td[1]/text()');
+const text3 = await page.locator('//tbody/tr[5]/td[3]/td[1]/text()');
+const text4 = await page.locator('//tbody/tr[7]/td[3]/td[1]/text()');
 
+console.log(text1);
+console.log(text2);  // In ra giá trị "0"
+console.log(text3);
+console.log(text4);
+*/
+ /*await page.locator('div[id = "demo-select-selfAssessment.0.self_assessment_score"]').click();
+ await page.locator('div[id="demo-select-selfAssessment.0.self_assessment_score"] >> div.ant-select-item-option:nth-child(2)').click();
+ await page.locator('body').click();
+*/
 
-
+const locator = await page.locator('//tbody/tr[2]/td[5]').innerText();
+console.log(locator);
+//const textreceived = await expect(locator).toHaveText('0.25');
+//console.log(textreceived);
+});
 
 
 
